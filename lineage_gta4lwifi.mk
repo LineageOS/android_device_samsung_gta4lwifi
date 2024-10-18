6dfaa967b7bd7dae52bf9e5d6cf5da3e86f57c75
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    gta4lwifi: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_gta4lwifi.mk
+++ lineage_gta4lwifi.mk
@@ -37,7 +37,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=gta4lwifixx \
-    PRIVATE_BUILD_DESC="gta4lwifixx-user 12 SP1A.210812.016 T500XXS7CXB1 release-keys"
-
-BUILD_FINGERPRINT := "samsung/gta4lwifixx/gta4lwifi:11/RP1A.200720.012/T500XXS7CXB1:user/release-keys"
+    BuildDesc="gta4lwifixx-user 12 SP1A.210812.016 T500XXS7CXB1 release-keys" \
+    BuildFingerprint=samsung/gta4lwifixx/gta4lwifi:11/RP1A.200720.012/T500XXS7CXB1:user/release-keys \
+    DeviceProduct=gta4lwifixx \
+    SystemName=gta4lwifixx
